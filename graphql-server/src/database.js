4b1ec47fb09ff734ff367c23c035d25fc09b6f9a
Santiago <sanchezgrisaless@gmail.com>
2021-12-02 06:32:25 -0500

    delete

--- graphql-server/src/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://admin:admin@cluster0.8vpll.mongodb.net/Master_Synergy?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log("Database is conected"))
-    .catch(e => console.log(e));
